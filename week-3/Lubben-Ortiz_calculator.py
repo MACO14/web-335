b98266e78b4a2fa5d96fda58e83479702dd5d013
Mackenzie Ortiz <mlubben1@gmail.com>
2024-01-24 11:09:46 -0500

    Add files via upload

---
+++ week-3/Lubben-Ortiz_calculator.py
@@ -0,0 +1,41 @@
+# Name: Mackenzie Lubben-Ortiz
+# Date: 1/24/2024
+# Assignment: calculator_py (Exercise 3.3)
+# Description: calculator 
+
+# Function to add two numbers
+
+x = 4
+y = 2
+
+def add(x, y):
+    return x + y
+
+sum = int(x) + int(y)
+print(sum)
+print("4 + 2 is 6")
+
+# Function to subtract two numbers
+
+def subtract(x, y):
+    return x - y
+total = int(x) - int(y)
+print(total)
+print("4 - 2 is 2")
+
+# Function to multiply two numbers
+    
+def multiply(x, y):
+    return x * y
+sum1 = int(x) * int(y)
+print(sum1)
+print("4 * 2 is 8")
+
+# Function to divide two numbers
+
+def divide(x, y):
+    return x / y
+total1 = int(x) / int(y)
+print(total1)
+print("4 / 2 is 2")
+
