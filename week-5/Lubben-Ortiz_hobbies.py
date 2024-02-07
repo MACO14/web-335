8f0a78ea87d02b758029b70efe6fff1edaeb1909
Mackenzie Ortiz <mlubben1@gmail.com>
2024-02-07 11:05:15 -0500

    Add files via upload

---
+++ week-5/Lubben-Ortiz_hobbies.py
@@ -0,0 +1,34 @@
+# Title: Hobbies
+## Name: Mackenzie Lubben-Ortiz
+### Date: 06 Feb 2024
+#### Description: hobbies.py
+
+# Creates a list of at least five hobbies
+
+hobbies = ["biking", "swimming", "running", "gaming", "painting"]
+for hobby in hobbies:
+    print(hobby)
+
+
+# Creates a list of days of the week 
+
+days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+for day in days: 
+    if day == "Sunday":
+        print("Congrats, it's the weekend and you don't have to work!")
+    elif day == "Saturday":
+        print("It's the weekend!")
+    elif day == "Monday":
+        print("It's Monday, a work day")   
+    elif day == "Tuesday":
+        print("It's Tuesday, a work day ")
+    elif day == "Wednesday":
+        print("It's Wednesday, a work day")
+    elif day == "Thursday":
+        print("It's Thursday, a work day")
+    elif day == "Friday":
+        print("It's Friday, a work day")
+    else: 
+        print(day)
+    
+    
